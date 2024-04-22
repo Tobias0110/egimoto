84a446357d18f2979fcadeb5f1c873b8a56cc14d
OE3TEC <Tobias0110@users.noreply.github.com>
2024-04-22 23:45:04 +0200

    Initial commit for mmdvm-mqtt support

---
+++ mmdvm-digital-voice-dashboard/index.js
@@ -0,0 +1,145 @@
+import express from 'express'
+import helmet from 'helmet'
+import SSE from 'express-sse'
+import compression from 'compression'
+import dotenv from 'dotenv'
+import url from 'node:url'
+import path from 'node:path'
+import { engine } from 'express-handlebars'
+import { mqttConnect } from './mqtt.js'
+import { readCallerIdNames } from './callerIdNames.js'
+import { readVersionNumber } from './version.js'
+
+dotenv.config()
+
+const currentDirectory= url.fileURLToPath( new URL('.', import.meta.url) )
+const maxPacketHistoryLength= parseInt(process.env.MAX_PACKET_HISTORY_LENGTH)
+const packetHistory= []
+
+function transmitPacket( packet ) {
+  stream.send( packet )
+
+  // Add packet to the history and delete old ones if necessary
+  packetHistory.push( packet )
+  while( packetHistory.length > maxPacketHistoryLength ) {
+    packetHistory.shift()
+  }
+}
+
+function packetsHaveSameCall( a, b ) {
+  return a.to === b.to && a.from === b.from
+}
+
+/**
+ * The following setup prevents calls from going on indefinitely when there is a mismatch of start and
+ * end packets. This can happen (frequently) when the system receives a start packet, but no end packet.
+ * However, as DMR only allows one speaker at a time, we can safely assume that a call has ended when a  
+ * start packet for a different one comes in. 
+ * This is a bit more complicated by the fact, that the DMR system can broadcast the same speaker on
+ * different time slots concurrently. Therefore, instead of only remembering the currently open call, we
+ * store a list of all calls started with the same 'from' and 'to' fields. Whenever an end packet comes
+ * in that matches a packet in the list, we remove it from the list. In case a start packet arrives that
+ * does not match the list, we assume that we lost at least one end packet and generate the end packets
+ * ourselves on the fly and empty the list. The new start packet is then again added to the list.
+ */
+const openCalls= []
+function handleOpenCalls( packet ) {
+  if( packet.action === 'start' ) {
+    // Close any other open start packets if they do not have the same 'from' and 'to' fields
+    // The list therefore only contains packets with the same fields, so we can just compare with the first item
+    if( openCalls.length && packet.action === 'start' && !packetsHaveSameCall(openCalls[0], packet) ) {
+      openCalls.forEach( startPacket => {
+        const stopPacket= {...startPacket}
+        stopPacket.time= new Date().toISOString()
+        stopPacket.action= 'end'
+        transmitPacket( stopPacket )
+        
+        console.log(`[Stream] Generated missing stop packet: from ${stopPacket.from} -> to ${stopPacket.to}`)
+      })
+
+      openCalls.length= 0
+    }
+    
+    // Remember this start packet for manually closing later if necessary
+    openCalls.push( packet )
+  }
+
+  // Remove start packets from the open list if an incoming end packet matches the 'from', 'to' and 'typ(e)' fields
+  if( openCalls.length && packet.action === 'end' && packetsHaveSameCall(openCalls[0], packet) ) {
+    for( let i= 0; i < openCalls.length; i++ ) {
+      const startPacket= openCalls[i]
+      if( (startPacket.typ || startPacket.type) === (packet.typ || packet.type) ) {
+        openCalls.splice(i, 1)
+        i--
+      }
+    }
+  }
+}
+
+// Read the caller id names CSV and connect to mqtt in parallel
+const [callerIdNames, client, versionNumber]= await Promise.all([ readCallerIdNames(), mqttConnect(), readVersionNumber(currentDirectory) ])
+
+const app = express()
+
+// Setup handlebars views directory and file extension
+app.engine('hbs', engine({defaultLayout: 'main', extname: '.hbs'}))
+app.set('view engine', 'hbs')
+app.set('views', path.join(currentDirectory, '/views'))
+
+app.locals.version= versionNumber
+app.locals.repeaterCallSign= process.env.REPEATER_CALL_SIGN
+
+// Add middleware functions
+app.use(helmet())
+app.use(compression())
+app.use(express.static(path.join(currentDirectory, '/static')))
+
+// Setup view routes
+app.get('/', (req, res) => res.render('home'))
+app.get('/about/en', (req, res) => res.render('about_en'))
+app.get('/about/de', (req, res) => res.render('about_de'))
+app.get('/faq/en', (req, res) => res.render('faq_en'))
+app.get('/faq/de', (req, res) => res.render('faq_de'))
+
+const stream= new SSE()
+app.get('/stream', stream.init)
+app.get('/history', (req, resp) => resp.send(packetHistory) )
+
+client?.on('message', (topic, payload) => {
+  if( topic !== process.env.MQTT_TOPIC ) {
+    return
+  }
+
+  const jsonString= payload.toString('utf8').trim()
+  if( !jsonString ) {
+    return
+  }
+
+  try {
+    // Parse the json into an object to add some additional fields
+    const packet= JSON.parse( jsonString )
+    const dmrMode= (packet.typ || packet.type || '').toLowerCase().indexOf('dmr') >= 0
+    packet.time= new Date().toISOString()
+    packet.fromName= dmrMode ? callerIdNames.get( parseInt(packet.from) ) || '' : ''
+
+    // Only translate private call (PC) caller ids to names
+    const [callType, toField]= packet.to.split(' ')
+    if( dmrMode && callType && callType.toUpperCase() === 'PC') {
+      const toFieldName= callerIdNames.get( parseInt(toField) )
+      packet.toName= toFieldName ? `${callType} ${toFieldName}` : ''
+    }
+
+    handleOpenCalls( packet )
+    transmitPacket( packet )
+
+  } catch( e ) {
+    console.error('Could not decode mqtt message', e)
+  }
+})
+
+console.log(`[HELLO] Running digital voice dashboard v${versionNumber}`)
+
+const port= parseInt(process.env.PORT)
+app.listen(port, () => {
+  console.log(`[HTTP] Server listening on ${port}`)
+})
---
+++ mmdvm-digital-voice-dashboard/static/main.js
@@ -0,0 +1,379 @@
+import langs from './lang.js'
+
+if( window.location.pathname === '/' ) {
+  const chronologicalCheckbox= document.getElementById('chronological-checkbox')
+  const keepEntriesCheckbox= document.getElementById('keep-entries-checkbox')
+
+  function showErrorModal( message ) {
+    document.getElementById('error-message').innerText= message
+    document.getElementById('error-modal').showModal()
+  }
+
+  function formatTime( date ) {
+    if( !(date instanceof Date) || Number.isNaN(date) ) {
+      return ''
+    }
+
+    const hours= `${date.getHours()}`.padStart(2, '0')
+    const minutes= `${date.getMinutes()}`.padStart(2, '0')
+    const seconds= `${date.getSeconds()}`.padStart(2, '0')
+
+    return `${hours}:${minutes}:${seconds}`
+  }
+
+  document.querySelectorAll('input[data-stored').forEach( input => {
+    const storedValue= localStorage.getItem(input.name)
+    if( input.type === 'radio' ) {
+      if( storedValue !== null ) {
+        input.checked= storedValue === input.value
+      }
+      input.addEventListener('change', e => localStorage.setItem(input.name, input.value))
+    } else {
+      if( storedValue !== null ) {
+        input.checked= storedValue !== 'false'
+      }
+      input.addEventListener('change', e => localStorage.setItem(input.name, input.checked))
+    }
+  })
+
+  let lang= null
+
+  // Setup language selector
+  document.querySelectorAll('input[type="radio"][name="lang"]').forEach( button => {
+    function update() {
+      if( !button.checked ) {
+        return
+      }
+
+      lang= langs[button.value]
+      if( !lang ) {
+        return
+      }
+
+      localStorage.setItem('lang', button.value)
+
+      document.querySelectorAll('*[data-lang]').forEach( elem => {
+        elem.innerText= lang[elem.getAttribute('data-lang')]
+      })
+    }
+
+    button.addEventListener('change', update)
+    update()
+  })
+
+  // Setup fade-out timer enable/disable button
+  keepEntriesCheckbox.addEventListener('change', e => {
+    if( keepEntriesCheckbox.checked ) {
+      Talker.forEach(table, talker => talker.clearFadeoutTimer())
+    } else {
+      Talker.forEach(table, talker => talker.setFadeoutTimer())
+    }
+  })
+
+  // Try to load the history from the server. Return an empty history on error
+  async function loadHistory() {
+    try {
+      const historyResp= await fetch('/history')
+      if( !historyResp.ok ) {
+        throw Error(`Server status was: ${historyResp.status}`)
+      }
+
+      const history= await historyResp.json()
+      if( !Array.isArray(history) ) {
+        throw Error('Not an array')
+      }
+
+      return history
+
+    } catch( e ) {
+      console.error('Could not load history:', e)
+      return []
+    }
+  }
+
+  class HistoryStream {
+    constructor( url, history= [] ) {
+      this._stream= new EventSource( url )
+      this._history= history
+      this.onPacket= null
+
+      this._stream.addEventListener('message', event => this._handlePacket(event))
+      this._stream.addEventListener('error', event => this._handleError(event))
+    }
+
+    _handlePacket( event ) {
+      try {
+        const packet= JSON.parse( event.data )
+        this._history.push( packet )
+        while( this._history.length > 10000 ) {
+          this._history.shift()
+        }
+
+        if( this.onPacket ) {
+          this.onPacket( packet )
+        }
+        
+      } catch( e ) {
+        console.error('Could not handle incoming packet:', e)
+      }
+    }
+
+    _handleError( event ) {
+      console.error('SSE error:', event)
+      showErrorModal('Lost connection to the server. Try reloading the page')
+    }
+
+    forEach( func ) {
+      this._history.forEach( func )
+    }
+  }
+
+  class Talker {
+    constructor( config ) {
+      this.startTime= -1
+      this.fadeOutTimer= null
+      this.callerId= config.from
+      this.mode= config.type || config.typ || ''
+      this.callerIdsLabel= ''
+      this.callerNamesLabel= ''
+      this.showsCallerNames= true
+
+      this.tableRow= document.createElement('tr')
+      this.tableRow._talkerInstance= this
+      
+      const activeElem= this.tableRow.appendChild( document.createElement('td') )
+      activeElem.classList.add('active')
+      activeElem.appendChild( document.createElement('span') )
+
+      const connectionElem= this.tableRow.appendChild( document.createElement('td') )
+      connectionElem.classList.add('connection')
+      connectionElem.appendChild( document.createElement('img') ).src= ''
+      connectionElem.appendChild( document.createElement('img') ).src= '/arrow.svg'
+      connectionElem.appendChild( document.createElement('img') ).src= '/online.svg'
+
+      this.tableRow.appendChild( document.createElement('td') )
+
+      const callerElem= this.tableRow.appendChild( document.createElement('td') )
+      callerElem.classList.add('caller')
+      callerElem.appendChild( document.createElement('img') ).addEventListener('click', () => {
+        this.displayCallerFieldNames( this.mode.toLowerCase().indexOf('dmr') < 0 || !this.showsCallerNames )
+      })
+      callerElem.appendChild( document.createElement('span') )
+      
+      const clockElem= this.tableRow.appendChild( document.createElement('td') )
+      clockElem.innerText= '-'
+
+      this.tableRow.appendChild( document.createElement('td') )
+
+      this.updateFromPacket( config )
+    }
+
+    updateFromPacket( config ) {
+      const {action, external, typ, type, from, fromName, to, toName, time: isoTime}= config
+
+      this.callerId= from
+      this.mode= type || typ || ''
+
+      this.activeElem.classList.toggle('active', action === 'start')
+      this.activeElem.classList.toggle('inactive', action === 'end')
+      this.activeElem.title= action === 'start' ? lang['active'] : lang['inactive']
+
+      this.connectionElem.title= external ? lang['network call'] : lang['rf call']
+      this.connectionElem.firstElementChild.src= external ? '/address.svg' : '/radio.svg'
+
+      this.modeElem.innerText= this.mode
+      this.callerNamesLabel= `${fromName || from} → ${toName || to}`
+      this.callerIdsLabel= `${from} → ${to}`
+      this.displayCallerFieldNames( this.showsCallerNames )
+
+      const time= new Date( isoTime )
+
+      if( action === 'start' ) {
+        if( this.startTime < 0 ) {
+          this.startTime= time.getTime()
+          this.clockElem.innerText= '0min 0s'
+          this.timestampElem.innerText= formatTime( time )
+          this.timestampElem.title= time.toISOString()
+        }
+
+        this.clearFadeoutTimer()
+
+      } else if( action === 'end' ) {
+        // Keep the clock value in chronological view
+        if( chronologicalCheckbox.checked ) {
+          this._updateClockToReference( time.getTime() )
+        } else {
+          this.clockElem.innerText= '—'
+        }
+
+        this.startTime= -1
+        this.setFadeoutTimer()
+
+        // If we do not know the start time, just use the end time
+        if( !this.timestampElem.innerText ) {
+          this.timestampElem.innerText= formatTime( time )
+          this.timestampElem.title= time.toISOString()
+        }
+      }
+    }
+
+    setFadeoutTimer() {
+      if( !this.fadeOutTimer && !keepEntriesCheckbox.checked ) {
+        this.fadeOutTimer= window.setTimeout( () => this.detach(), 10* 60* 1000)
+      }
+    }
+
+    clearFadeoutTimer() {
+      if( this.fadeOutTimer && (this.startTime < 0) ) {
+        window.clearTimeout( this.fadeOutTimer )
+        this.fadeOutTimer= null
+      }
+    }
+
+    updateClock() {
+      this._updateClockToReference( Date.now() )
+    }
+
+    _updateClockToReference( endTime ) {
+      if( this.startTime < 0 ) {
+        return
+      }
+
+      const time= Math.round( (endTime- this.startTime) / 1000 )
+      const secs= Math.floor( time % 60 )
+      const mins= Math.floor( time / 60 )
+      this.clockElem.innerText= `${mins}min ${secs}s`
+    }
+
+    displayCallerFieldNames( showCallerNames ) {
+      const [icon, span]= this.callerElem.children
+      if( this.showsCallerNames= showCallerNames ) {
+        icon.src= '/tag.svg'
+        icon.title= lang['show ids']
+        span.innerText= this.callerNamesLabel
+        span.title= this.callerIdsLabel
+
+      } else {
+        icon.src= '/hashtag.svg'
+        icon.title= lang['show names']
+        span.innerText= this.callerIdsLabel
+        span.title= this.callerNamesLabel
+      }
+    }
+
+    attach( table ) {
+      table.prepend( this.tableRow )
+    }
+
+    async detach() {
+      const fadeOut= [
+        {filter: 'opacity(1)'},
+        {filter: 'opacity(0.4)'}
+      ]
+      await this.tableRow.animate(fadeOut, {duration: 500, iterations: 1}).finished
+      this.tableRow.parentElement.removeChild( this.tableRow )
+    }
+
+    static Break= {}
+
+    static forEach( table, fn ) {
+      const rows= table.rows
+      for( let i= 0; i!== rows.length; i++ ) {
+        const talker= rows[i]._talkerInstance
+        if( talker instanceof Talker ) {
+          if( fn( talker ) === Talker.Break ) {
+            return
+          }
+        }
+      }
+    }
+
+    static findFirst( table, fn ) {
+      let talker= null
+      Talker.forEach( table, t => {
+        if( fn( t ) ) {
+          talker= t
+          return Talker.Break
+        }
+      })
+
+      return talker
+    }
+
+    static findByCallerId( table, id ) {
+      return this.findFirst( table, t => t.callerId === id )
+    }
+
+    static findByCallerIdAndMode( table, id, mode ) {
+      return this.findFirst( table, t => t.callerId === id && t.mode === mode )
+    }
+
+    static findActiveByCallerIdAndMode( table, id, mode ) {
+      return this.findFirst( table, t => t.callerId === id && t.mode === mode && t.startTime >= 0 )
+    }
+
+    get activeElem() { return this.tableRow.cells[0] }
+    get connectionElem() { return this.tableRow.cells[1] }
+    get modeElem() { return this.tableRow.cells[2] }
+    get callerElem() { return this.tableRow.cells[3] }
+    get clockElem() { return this.tableRow.cells[4] }
+    get timestampElem() { return this.tableRow.cells[5] }
+  }
+
+  // Get the main table and setup the clock update timer (update every 500ms)
+  const table= document.querySelector('main table').tBodies[0]
+  setInterval(() => Talker.forEach(table, talker => talker.updateClock()), 500)
+
+  function clearTable() {
+    // Delete all rows except the very last one (this is the table empty message)
+    while( table.rows.length > 1 ) {
+      table.deleteRow( 0 )
+    }
+  }
+
+  function consumePacket( packet ) {
+    // Group everything in grouped mode by user id
+    if( !chronologicalCheckbox.checked ) {
+      // Try to find a row with the same caller id
+      const existingTalker= Talker.findByCallerId( table, packet.from )
+      if( existingTalker ) {
+        existingTalker.updateFromPacket( packet )
+        return
+      }
+    // Chronological mode
+    } else {
+      // Group end packets with start packets in by user id and mode
+      if( packet.action === 'end' ) {
+        const existingTalker= Talker.findByCallerIdAndMode( table, packet.from, packet.type || packet.typ )
+        if( existingTalker ) {
+          existingTalker.updateFromPacket( packet )
+          return
+        }
+      // Group start packets of the same call to prevent multiple active entries
+      } else if( packet.action === 'start' ) {
+        const existingTalker= Talker.findActiveByCallerIdAndMode( table, packet.from, packet.type || packet.typ )
+        if( existingTalker ) {
+          existingTalker.updateFromPacket( packet )
+          return
+        }
+      }
+    }
+
+    // Create a new table row
+    const newTalker= new Talker( packet )
+    newTalker.attach( table )
+  }
+
+  // Connect to the server-sent-event source
+  const history= await loadHistory()
+  const stream= new HistoryStream('/stream', history)
+
+  stream.forEach( consumePacket )
+  stream.onPacket= consumePacket
+
+  // Setup chronological/grouped view toggle button
+  chronologicalCheckbox.addEventListener('change', e => {
+    clearTable()
+    stream.forEach( consumePacket )
+  })
+}
